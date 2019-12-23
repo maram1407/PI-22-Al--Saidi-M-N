44e0f304525e756438f645a56f05e95c1b3cf40a
maram1407 <55602198+maram1407@users.noreply.github.com>
2019-12-23 08:16:39 -0800

    Update FormParking.Designer.cs

--- FormTractor/FormTractor/FormParking.Designer.cs
+++ FormTractor/FormTractor/FormParking.Designer.cs
@@ -88,7 +88,7 @@
             this.groupBox1.Size = new System.Drawing.Size(161, 224);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "действия";
             // 
             // pictureBoxTakeTractor
             // 
