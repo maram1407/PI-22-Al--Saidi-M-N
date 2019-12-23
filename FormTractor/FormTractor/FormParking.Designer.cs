0bb8aa76ebbad0b87137541796a033f693c8c091
maram1407 <55602198+maram1407@users.noreply.github.com>
2019-12-23 09:16:02 -0800

    Update FormParking.Designer.cs

--- FormTractor/FormTractor/FormParking.Designer.cs
+++ FormTractor/FormTractor/FormParking.Designer.cs
@@ -81,7 +81,7 @@
             // 
             this.groupBox1.Controls.Add(this.pictureBoxTakeTractor);
             this.groupBox1.Controls.Add(this.buttonTakeTractor);
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.labelPlace);
             this.groupBox1.Controls.Add(this.maskedTextBox);
             this.groupBox1.Location = new System.Drawing.Point(651, 226);
             this.groupBox1.Name = "groupBox1";
