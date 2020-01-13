f0ee21935556e2e07ca584733c8420105c3a4d8f
maram1407 <55602198+maram1407@users.noreply.github.com>
2020-01-13 01:52:32 -0800

    Update FormParking.Designer.cs

--- FormTractor/FormTractor/FormParking.Designer.cs
+++ FormTractor/FormTractor/FormParking.Designer.cs
@@ -146,7 +146,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(815, 450);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelLevel);
             this.Controls.Add(this.listBoxLevels);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonSetTractorBllduser);
